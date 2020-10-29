cd9e4df4702d0ad6ac0026c0c715a2287b4eccb3
Calvin Griffin <cgriffin332@gmail.com>
2020-10-28 22:02:17 -0400

    basic bootstrap table

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -3,33 +3,28 @@ import React, { Component } from "react";
 class Table extends Component {
   render() {
     return (
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Firstname</th>
-              <th>Lastname</th>
-              <th>Phone</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <img
-                  alt="employee"
-                  src="http://www.fillmurray.com/200/200"
-                ></img>
-              </td>
-              <td>Bill</td>
-              <td>Murray</td>
-              <td>123-456-7890</td>
-              <td>www.billmurray@gmail.com</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img alt="employee" src="http://www.fillmurray.com/100/100"></img>
+            </td>
+            <td>Bill</td>
+            <td>Murray</td>
+            <td>123-456-7890</td>
+            <td>www.billmurray@gmail.com</td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
